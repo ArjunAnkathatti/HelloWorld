40f43269fe58447680bb9336309102a6745563df
Arjun Ankathatti <acarjungowda@icloud.com>
2024-12-28 14:25:26 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Arjun Ankathatti Chandrashekara on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
